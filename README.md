c060e59001c1cac5574ce74d5be8453fe1c4f493
ryanwoodsmall <rwoodsmall@gmail.com>
2023-11-18 09:39:48 +0000

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# oksh
+
+This is a Habitat build of oksh, a portable Korn shell from OpenBSD.
+The build is statically compiled with musl libc; no features are enabled.
+This build only aims to be a workable standalone, small, fast `/bin/ksh` shell;
+interactive use will likely be unpleasant.
+
+## links
+- https://github.com/ibara/oksh
---
+++ plan.sh
@@ -0,0 +1,51 @@
+pkg_name="oksh"
+pkg_origin="ryanwoodsmall"
+pkg_version="7.3"
+pkg_license=("BSD")
+pkg_maintainer="ryanwoodsmall <rwoodsmall@gmail.com>"
+pkg_description="Portable OpenBSD ksh, based on the Public Domain Korn Shell (pdksh)."
+pkg_upstream_url="https://github.com/ibara/oksh"
+pkg_dirname="${pkg_name}-${pkg_version}"
+pkg_filename="${pkg_dirname}.tar.gz"
+pkg_source="https://github.com/ibara/oksh/releases/download/${pkg_dirname}/${pkg_filename}"
+pkg_shasum="9f176ff6841435a55f27bfd3ebbfc951c8cca6fdf3638f0123f44617e3992f93"
+pkg_build_deps=("core/gcc" "core/musl" "core/file" "core/busybox-static" "core/make")
+pkg_bin_dirs=("bin")
+
+DO_CHECK=1
+
+do_build() {
+  cd "${SRC_PATH}" || exit 1
+  local CFLAGS="-Os -g0 -Wl,-s -Wl,-static -DEMACS"
+  ./configure \
+    --prefix="${pkg_prefix}" \
+    --bindir="${pkg_prefix}/bin" \
+    --mandir="${pkg_prefix}/share/man" \
+    --cc="$(pkg_path_for core/musl)/bin/musl-gcc"\
+    --cflags="${CFLAGS}" \
+    --disable-curses \
+    --enable-small \
+    --enable-static
+  make LDFLAGS='-s -static' CFLAGS="${CFLAGS}" CPPFLAGS=
+}
+
+do_install() {
+  cd "${SRC_PATH}" || exit 1
+  make install
+  ln -s oksh "${pkg_prefix}/bin/ksh"
+}
+
+do_check() {
+  cd "${SRC_PATH}" || exit 1
+  build_line "checking that oksh is static"
+  file oksh | grep 'ELF.*static'
+  build_line "checking that oksh can run something"
+  build_line "set internal..."
+  env -i ./oksh -c set
+  build_line "KSH environment vars..."
+  env -i PATH=$(pkg_path_for core/busybox-static)/bin ./oksh -c 'echo ${KSH_VERSION} ; echo ${OKSH_VERSION}' > habtest.out
+  grep KSH habtest.out
+  grep oksh habtest.out
+  build_line "env from busybox..."
+  env -i PATH=$(pkg_path_for core/busybox-static)/bin ./oksh -c env
+}
